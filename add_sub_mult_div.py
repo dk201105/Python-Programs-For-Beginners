c9b5c4e6f2bc51f63aeb9da59dae4b91962c9b3b
dk201105 <dkjee2023@gmail.com>
2025-01-21 21:56:43 +0530

    Add files via upload

---
+++ add_sub_mult_div.py
@@ -0,0 +1,15 @@
+#write a program to calculate addition, subtraction, multiplication, division
+n1=int(input("Enter number 1 "))
+n2=int(input("Enter number 2 "))
+#addition
+a=n1+n2
+print("sum =", a)
+#subtraction
+s=n1-n2
+print("difference =", s)
+#multiplication
+m=n1*n2
+print("product =", m)
+#division
+d=n1/n2
+print("quotient=", d)
